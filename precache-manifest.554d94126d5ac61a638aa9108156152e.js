70c9513f1b27dffa8279188833bbea3451c32a71
Paul louis <paullouis@Pauls-MacBook-Air.local>
2020-06-08 19:40:36 +0530

    Updates

---
+++ precache-manifest.554d94126d5ac61a638aa9108156152e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea5cf2cf3e943ab5d3c9d0e2481da1b",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "8a0a3823c7252e9cbfbe",
+    "url": "/youtube-clone/static/js/2.1607d4d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.1607d4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3605926c5d63cb258f",
+    "url": "/youtube-clone/static/js/main.4bc0e278.chunk.js"
+  },
+  {
+    "revision": "413e03f976fe2fb1f22f",
+    "url": "/youtube-clone/static/js/runtime-main.19dead21.js"
+  }
+]);
